70c884c321310dbe05a1c04c04919f56de72f3cd
unknown <Admin@EMLP-MUM0016.aurum.com>
2022-06-18 04:12:56 +0530

    Another commit to check the workflow

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>If You can see this text that means your setup is successfull</p>
         <a
           className="App-link"
           href="https://reactjs.org"
