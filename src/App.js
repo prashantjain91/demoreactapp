a12f3e09850e4d52340a50e2d739c0681e00e001
unknown <Admin@EMLP-MUM0016.aurum.com>
2022-06-18 13:54:31 +0530

    New CSS file is added for app container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import "./App.css";
 const App = () => (
-<div>
+<div className="mainappcontainer">
 <h2>Hello From Sagar</h2>
 <p>Learning React With Prashant</p>  
 </div>
